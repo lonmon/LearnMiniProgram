bfb4fe34656e622332af40066a2885d1512b9879
lonmon <505386930@qq.com>
2019-12-30 11:58:14 +0800

    小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,31 @@
 // pages/home/home.js
+
+//page()函数
+//  {}对象自变量
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    name:'lonmon',
+    age:18,
+    students:[
+      { id: 110, name: 'Tomes', age: 20 },
+      { id: 111, name: 'Hobby', age: 23 },
+      { id: 112, name: 'Alice', age: 33 },
+      { id: 113, name: 'Bob', age: 30 },
+    ],
+    counter:0,
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtnClick(){
+    // false this.data.counter+=1;
+    // this.setData()
+    this.setData({
+      counter:this.data.counter+1,
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction(){
+    this.setData({
+      counter: this.data.counter - 1,
+    })
   }
 })

eb69fb5d5b89a3a76f59516697d0fa019f04ab72
longway <43658905+lonmon@users.noreply.github.com>
2019-12-30 14:55:28 +0800

    Update home.js

--- pages/home/home.js
+++ pages/home/home.js
@@ -4,28 +4,5 @@
 //  {}对象自变量
 
 Page({
-  data:{
-    name:'lonmon',
-    age:18,
-    students:[
-      { id: 110, name: 'Tomes', age: 20 },
-      { id: 111, name: 'Hobby', age: 23 },
-      { id: 112, name: 'Alice', age: 33 },
-      { id: 113, name: 'Bob', age: 30 },
-    ],
-    counter:0,
-  },
 
-  handleBtnClick(){
-    // false this.data.counter+=1;
-    // this.setData()
-    this.setData({
-      counter:this.data.counter+1,
-    })
-  },
-  handleSubtraction(){
-    this.setData({
-      counter: this.data.counter - 1,
-    })
-  }
 })
